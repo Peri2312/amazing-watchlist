f1d0863ca0db9d19fdf17de43c35a0da8a182005
Peri2312 <peripatel2312@gmail.com>
2021-02-28 12:49:19 -0500

    search button done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { SearchComponent } from './search/search.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { ShowInfoComponent } from './show-info/show-info.component';
 
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +26,13 @@ import { ShowInfoComponent } from './show-info/show-info.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatCardModuleg
   ],
   providers: [],
   bootstrap: [AppComponent]
